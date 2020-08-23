262fb9f4222a8c7e1573d07fdc5bd35a76cdd70d
kathir <chengathir@gmail.com>
2020-08-23 21:06:02 +0530

    Second Commit

--- Hello.java
+++ Hello.java
@@ -4,6 +4,7 @@ public class Hello
 	public static void main(String args[])
 	{
          	System.out.println("Hello, World from GIT!");
-		System.out.println("Welcome to Cloud ");
+		System.out.println("Welcome to Cloud Computing ");
+		System.out.println("Changes after First Commit");
 	}
 }
